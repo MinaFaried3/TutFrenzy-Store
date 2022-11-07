fb15edde86a255d8bcca1de0bd391bbd1bdca150
Mina  Faried <mina.el.dihabi3.6.9@gmail.com>
2022-11-07 15:31:45 +0200

    add network files and login models

---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:frenzy_store/app/constants.dart';
+import 'package:retrofit/http.dart';
+
+import '../response/responses.dart';
+
+part 'app_api.g.dart';
+
+@RestApi(baseUrl: Constants.baseUrl)
+abstract class AppServiceClient {
+  factory AppServiceClient(Dio dio, {String? baseUrl}) = _AppServiceClient;
+
+  @POST("/customers/login")
+  Future<AuthenticationResponse> login(
+    @Field("email") String email,
+    @Field("password") String password,
+  );
+}
---
+++ lib/domain/models/login_model.dart
@@ -0,0 +1,38 @@
+import 'package:equatable/equatable.dart';
+
+class Customer extends Equatable {
+  final String id;
+  final String name;
+  final int notificationNumber;
+
+  const Customer(
+      {required this.id, required this.name, required this.notificationNumber});
+
+  @override
+  List<Object> get props => [id, name, notificationNumber];
+}
+
+class Contacts extends Equatable {
+  final String phone;
+  final String email;
+  final String link;
+
+  const Contacts(
+      {required this.phone, required this.email, required this.link});
+
+  @override
+  List<Object> get props => [phone, email, link];
+}
+
+class Authentication extends Equatable {
+  final Customer customer;
+  final Contacts contacts;
+
+  const Authentication({
+    required this.customer,
+    required this.contacts,
+  });
+
+  @override
+  List<Object> get props => [customer, contacts];
+}
