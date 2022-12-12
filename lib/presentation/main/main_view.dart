e85cd46abe861d8595b087a3e542eea00dae291e
Mina  Faried <mina.el.dihabi3.6.9@gmail.com>
2022-12-12 20:19:34 +0200

    start working on main screen

--- lib/presentation/main/main_view.dart
+++ lib/presentation/main/main_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:frenzy_store/presentation/main/pages/home_page.dart';
+import 'package:frenzy_store/presentation/main/pages/notification_page.dart';
+import 'package:frenzy_store/presentation/main/pages/search_page.dart';
+import 'package:frenzy_store/presentation/main/pages/settings_page.dart';
+import 'package:frenzy_store/presentation/resources/strings_manager.dart';
 
 class MainView extends StatefulWidget {
   const MainView({Key? key}) : super(key: key);
@@ -8,8 +13,26 @@ class MainView extends StatefulWidget {
 }
 
 class _MainViewState extends State<MainView> {
+  final List<Widget> _pages = [
+    const HomePage(),
+    const SearchPage(),
+    const NotificationPage(),
+    const SettingsPage()
+  ];
+
+  String _title = AppStrings.home;
+  int _currentPage = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          _title,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+      ),
+      body: _pages[_currentPage],
+    );
   }
 }
