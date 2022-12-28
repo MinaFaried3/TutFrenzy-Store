c995898e09307837bd84eb48a91eaeab702329b2
Mina  Faried <mina.el.dihabi3.6.9@gmail.com>
2022-12-28 15:19:41 +0200

    settings ui

--- lib/presentation/main/pages/settings/settings_page.dart
+++ lib/presentation/main/pages/settings/settings_page.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:frenzy_store/app/app_prefs.dart';
+import 'package:frenzy_store/app/dependency_injection.dart';
+import 'package:frenzy_store/data/data_source/local_data_source.dart';
+import 'package:frenzy_store/presentation/resources/assets_manger.dart';
+import 'package:frenzy_store/presentation/resources/routes_manager.dart';
 import 'package:frenzy_store/presentation/resources/strings_manager.dart';
+import 'package:frenzy_store/presentation/resources/values_manager.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -9,10 +16,75 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  final AppPreferences _appPreferences = getItInstance<AppPreferences>();
+  final LocalDataSource _localDataSource = getItInstance<LocalDataSource>();
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(AppStrings.settings),
+    return Center(
+      child: ListView(
+        padding: const EdgeInsets.all(AppPadding.p8),
+        children: [
+          ListTile(
+            leading: SvgPicture.asset(ImageAssets.changeLangIc),
+            title: Text(AppStrings.changeLanguage,
+                style: Theme.of(context).textTheme.bodyLarge),
+            trailing: SvgPicture.asset(ImageAssets.rightArrowSettingsIc),
+            onTap: () {
+              _changeLanguage();
+            },
+          ),
+          ListTile(
+            leading: SvgPicture.asset(ImageAssets.contactUsIc),
+            title: Text(AppStrings.contactUs,
+                style: Theme.of(context).textTheme.bodyLarge),
+            trailing: SvgPicture.asset(ImageAssets.rightArrowSettingsIc),
+            onTap: () {
+              _contactUs();
+            },
+          ),
+          ListTile(
+            leading: SvgPicture.asset(ImageAssets.inviteFriendsIc),
+            title: Text(AppStrings.inviteYourFriends,
+                style: Theme.of(context).textTheme.bodyLarge),
+            trailing: SvgPicture.asset(ImageAssets.rightArrowSettingsIc),
+            onTap: () {
+              _inviteFriends();
+            },
+          ),
+          ListTile(
+            leading: SvgPicture.asset(ImageAssets.logoutIc),
+            title: Text(AppStrings.logout,
+                style: Theme.of(context).textTheme.bodyLarge),
+            trailing: SvgPicture.asset(ImageAssets.rightArrowSettingsIc),
+            onTap: () {
+              _logout();
+            },
+          )
+        ],
+      ),
     );
+  }
+
+  _changeLanguage() {
+    // i will implement it later
+  }
+
+  _contactUs() {
+    // its a task for you to open any webpage using URL
+  }
+
+  _inviteFriends() {
+    // its a task for you to share app name to friends
+  }
+
+  _logout() {
+    // app prefs make that user logged out
+    _appPreferences.logout();
+
+    // clear cache of logged out user
+    _localDataSource.clearCache();
+
+    // navigate to login screen
+    Navigator.pushReplacementNamed(context, Routes.loginRoute);
   }
 }
